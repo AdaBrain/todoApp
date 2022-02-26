6b4cd767c4483c4a077dad305fdaf837c2127ba5
adadesions <adadesions@gmail.com>
2022-02-26 14:51:08 +0700

    Add Task Card

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@
                 name="input-7-4"
                 label="Please type your important tasks!"
                 value=""
+                v-model="taskContent"
               ></v-textarea>
             </v-row>
             <v-row>
@@ -18,10 +19,26 @@
                 block
                 elevation="12"
                 large
+                color="#1E7C4B"
+                @click="onClickSave"
               >
                 Save
               </v-btn>
             </v-row>
+          </v-card-text>
+          <v-card-subtitle>
+            {{ taskContent }} <br />
+            {{ taskList }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" v-for="(task, i) in taskList" :key="i">
+        <v-card elevation="13" outlined> 
+          <v-card-text>
+            {{ task }}
           </v-card-text>
         </v-card>
       </v-col>
@@ -32,5 +49,19 @@
 <script>
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      taskContent: "",
+      taskList: [],
+    };
+  },
+  methods: {
+    onClickSave() {
+      if (this.taskContent !== "") {
+        this.taskList.push(this.taskContent);
+        this.taskContent = "";
+      }
+    },
+  },
 };
 </script>
